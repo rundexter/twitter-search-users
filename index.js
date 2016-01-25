eec55eaa365d94703a5d098d5eb89087d97b1f03
Alexander Zaretskii <al.zaretskii@gmail.com>
2016-01-25 20:01:53 +0200

    fix

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 var Twit = require('twit');
 var _ = require('lodash');
 
-var mapAuthOptionsEnv = {
-    'twitter_consumer_key': 'consumer_key',
-    'twitter_consumer_secret': 'consumer_secret',
-    'twitter_access_token': 'access_token',
-    'twitter_access_token_secret': 'access_token_secret'
-};
-
 var pickResult = {
     'screen_name': 'screen_name',
     'url': 'url',
@@ -16,20 +9,6 @@ var pickResult = {
 };
 
 module.exports = {
-    /**
-     * Run main twitter function.
-     *
-     * @param authOptions
-     * @param params
-     * @param callback
-     */
-    twitterMain: function (authOptions, params, callback) {
-        var twitter = new Twit(authOptions);
-
-        // Follow befriended
-        twitter.get('users/search', params, callback);
-    },
-
     /**
      * Return pick result.
      *
@@ -57,44 +36,28 @@ module.exports = {
     },
 
     /**
-     * Get Auth options from Environment.
-     *
-     * @param dexter
-     * @returns {{}}
-     */
-    authOptions: function (dexter) {
-        // twitter auth property
-        var authOptions = {};
-
-        _.map(mapAuthOptionsEnv, function (authOpt, twitterOpt) {
-            if(dexter.environment(twitterOpt)) {
-                // get auth property
-                authOptions[authOpt] = dexter.environment(twitterOpt);
-            } else {
-                // catch no-arguments message
-                this.fail('A ' + twitterOpt + ' environment variable is required for this module');
-            }
-        }, this);
-
-        return authOptions;
-    },
-
-    /**
      * Allows the authenticating users to follow the user specified in the ID parameter.
      *
      * @param {AppStep} step Accessor for the configuration for the step using this module.  Use step.input('{key}') to retrieve input data.
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
+        var credentials = dexter.provider('twitter').credentials(),
+            twitter = new Twit({
+                access_token: credentials.access_token,
+                access_token_secret: credentials.access_token_secret,
+                consumer_key: credentials.consumer_key,
+                consumer_secret: credentials.consumer_secret
+            });
 
-        this.twitterMain(this.authOptions(dexter), step.inputs(), function (error, twitterResult) {
+        twitter.get('users/search', step.inputs(), function (error, twitterResult) {
             if (error) {
                 // if error - send message
                 this.fail(error);
+            } else {
+                // return befriendedInfo
+                this.complete(this.pickResult(twitterResult));
             }
-
-            // return befriendedInfo
-            this.complete(this.pickResult(twitterResult));
         }.bind(this));
     }
 };
